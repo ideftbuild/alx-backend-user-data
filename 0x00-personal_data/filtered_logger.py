81b544f0fdb82773f4473d419c3fc3547a2a86de
ideftbuild <a.akanSWE@gamil.com>
2024-11-11 18:56:50 +0100

    feat: Add RedactingFormatter class for log redaction

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module: filtered_logger"""
 import re
+import logging
 from typing import List
 
 
@@ -12,3 +13,23 @@ def filter_datum(
     pattern: str = f'({("|").join(fields)})=(.*?)(?={separator}|$)'
     # Substitute the field values with the redaction string
     return re.sub(fr'{pattern}', f'\\1={redaction}', message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List):
+        self.fields: List = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Convert a LogRecord object to a redacted output"""
+        original_message: str = super(RedactingFormatter, self).format(record)
+        return filter_datum(
+            self.fields, self.REDACTION, original_message, self.SEPARATOR
+        )
