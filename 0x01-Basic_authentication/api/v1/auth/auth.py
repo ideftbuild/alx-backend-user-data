595fd4994ba86c6bdcd0ee749f1520e3fed640ce
ideftbuild <a.akanSWE@gamil.com>
2024-11-14 17:28:10 +0100

    add documentation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python3
+"""Module: auth"""
 from flask import request
 from typing import TypeVar, List
 
 
 class Auth:
+    """Authentication class"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ """
+        """
+        Checks if a user require authentication or not
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
-        """ """
+        """
+        Retrieves information from the authorization header
+        """
         return request
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ """
+        """
+        The current authenticated user
+        """
         return request
