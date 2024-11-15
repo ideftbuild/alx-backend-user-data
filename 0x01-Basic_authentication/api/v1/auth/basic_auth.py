684a184f24fbe4cfb162905ff3d8ea802715d06d
ideftbuild <a.akanSWE@gamil.com>
2024-11-15 17:16:18 +0100

    feat: Implement method that extracts the Base64 part of the authentication header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,21 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Authentication class"""
-    pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str
+    ) -> str:
+        """
+        Extracts the Base64 part of the Authorization header.
+
+        :param authorization_header: The Authorization header string.
+        :return: The Base64 encoded part of the Authorization header,
+        or None if invalid.
+        """
+        if not authorization_header:
+            return None
+        elif not isinstance(authorization_header, str):
+            return None
+        elif not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[6:]
