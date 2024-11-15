a3ad003ea659022007cbcb9005c587787f446f88
ideftbuild <a.akanSWE@gamil.com>
2024-11-15 15:25:56 +0100

    feat: Implement before_request handler for request authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,12 +6,38 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+if getenv('AUTH_TYPE') == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def authenticate_request():
+    """
+    Authenticate request by checking the path, authentication header,
+    and the current user.
+
+    :return: None if authentication is not required or the path is excluded.
+    """
+    if auth is None:
+        return
+    excluded_path = [
+        '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'
+    ]
+    if not auth.require_auth(request.path, excluded_path):
+        return
+
+    if not auth.authorization_header(request):
+        abort(401)
+    if not auth.current_user(request):
+        abort(403)
 
 
 @app.errorhandler(404)
