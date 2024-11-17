3a0ddf4b11dadc998eb6dde3341422a41993aa79
ideftbuild <a.akanSWE@gamil.com>
2024-11-17 05:12:35 +0100

    feat: Implement in-memory session ID storage and session creation method

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,4 +5,19 @@ from api.v1.auth.auth import Auth
 
 class SessionAuth(Auth):
     """Session based authentication"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a session ID for a user ID
+
+        :param user_id: The user ID
+        :return: The session ID
+        """
+        if not user_id or not isinstance(user_id, str):
+            return None
+        from uuid import uuid4
+
+        session = str(uuid4())
+        self.user_id_by_session_id[session] = user_id
+        return session
