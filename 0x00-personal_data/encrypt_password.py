d8b85060120c10390fb263199128c4604fba5ddd
ideftbuild <a.akanSWE@gamil.com>
2024-11-20 17:09:47 +0100

    feat: add `is_valid` function

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 """Module: encrypt_password"""
+from functools import wraps
+
+import bcrypt
 
 
 def hash_password(password: str) -> bytes:
     """Returns a salted, hashed password, which is a byte string."""
-    import bcrypt
 
     password = password.encode('utf-8')
     salt = bcrypt.gensalt()
 
     return bcrypt.hashpw(password, salt)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validates whether the provided password matches the hashed password."""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
