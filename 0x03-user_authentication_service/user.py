4f738cdd8bb027ae81bd6d27015fd2f892fa1376
ideftbuild <a.akanSWE@gamil.com>
2024-11-21 02:43:16 +0100

    feat: Implement User model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module: user"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    """User Model"""
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(256), nullable=False)
+    hashed_password = Column(String(256))
+    reset_token = Column(String(256))
