525b65fe5a3f175e2105b666bce40562bf610c1a
ideftbuild <a.akanSWE@gamil.com>
2024-11-21 15:17:06 +0100

    feat: Add `DB` class and `add_user` method

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db")
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+        self.__session = self._session
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Save user to the database"""
+        user = User(email=email, hashed_password=hashed_password)
+        self.__session.add(user)
+        self.__session.commit()
+        return user
