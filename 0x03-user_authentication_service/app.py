7dc2b9c38b709e5ea75547b3c247ae538bf1ade4
ideftbuild <a.akanSWE@gamil.com>
2024-11-22 15:30:46 +0100

    feat: Add `/users` route for registering new user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """Module: app"""
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+auth = Auth()
 
 
 @app.route('/', strict_slashes=False)
@@ -13,5 +15,18 @@ def home():
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register_user():
+    """Register new user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        auth.register_user(email, password)
+        return jsonify({'email': email, 'message': 'user created'})
+    except ValueError:
+        return jsonify({'message': 'email already registered'}), 400
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
