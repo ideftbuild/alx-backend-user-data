fbddde16f7459ce199392cbdaaea05ad0e925c44
ideftbuild <a.akanSWE@gamil.com>
2024-11-22 15:02:36 +0100

    feat: Add `Auth` class and `register_user` method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 """Module: auth"""
+from sqlalchemy.orm.exc import NoResultFound
+
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -7,3 +11,21 @@ def _hash_password(password: str) -> bytes:
     import bcrypt
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Adds a new User record to the database."""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            return self._db.add_user(
+                email=email, hashed_password=_hash_password(password)
+            )
