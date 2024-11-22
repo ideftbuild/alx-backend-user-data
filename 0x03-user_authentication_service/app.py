0e3c7ca997517ba4c01bd1d088317fb15d48ae70
ideftbuild <a.akanSWE@gamil.com>
2024-11-22 16:41:51 +0100

    feat: Add session-based authentication to /sessions route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Module: app"""
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -26,6 +26,20 @@ def register_user():
         return jsonify({'email': email, 'message': 'user created'})
     except ValueError:
         return jsonify({'message': 'email already registered'}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """Authenticate user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not auth.valid_login(email, password):
+        abort(401)
+
+    response = jsonify({'email': email, 'message': 'logged in'})
+    response.set_cookie('session_id', auth.create_session(email))
+    return response
 
 
 if __name__ == '__main__':
